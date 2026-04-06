37aae9f9098897cbb9243a8b1b46bcb6ad43518d
tanner-doorlist <tanner@doorlist.app>
2026-04-06 15:52:16 -0400

    fix: correct arm64 SHA256 for 1.0.2

--- Casks/study-notifier.rb
+++ Casks/study-notifier.rb
@@ -3,7 +3,7 @@ cask "study-notifier" do
 
   if Hardware::CPU.arm?
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-arm64.dmg"
-    sha256 "aea3fa5393a47074808c618b7f664d99c87b50a3fc60119a4f6acbf88fea309e"
+    sha256 "b54b4e3c9ad36b88490197124edf98960e3be777285266e8cb8c3200a86699ee"
   else
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-x64.dmg"
     sha256 "b535c75fd962c11dde6813a2e31639165c1bb264562fb56b63764b9f86b1e9ab"

8b7ba12a3b734e013eb21d5600451517d25a03f9
tanner-doorlist <tanner@doorlist.app>
2026-04-06 16:05:30 -0400

    Update study-notifier to 1.0.3

--- Casks/study-notifier.rb
+++ Casks/study-notifier.rb
@@ -1,12 +1,12 @@
 cask "study-notifier" do
-  version "1.0.2"
+  version "1.0.3"
 
   if Hardware::CPU.arm?
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-arm64.dmg"
-    sha256 "b54b4e3c9ad36b88490197124edf98960e3be777285266e8cb8c3200a86699ee"
+    sha256 "629e014ebbdeccac3826004d57ba6893faee5a776f269227399d5d0fa85787b4"
   else
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-x64.dmg"
-    sha256 "b535c75fd962c11dde6813a2e31639165c1bb264562fb56b63764b9f86b1e9ab"
+    sha256 "a337e2d5b735978e2e6a4d821668f0a116d831d100109137dfcfff0f9967f159"
   end
 
   name "Study Notifier"

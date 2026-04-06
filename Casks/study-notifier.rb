2862a02bd4c90e7872a97fad130042b1359c850c
tanner-doorlist <tanner@doorlist.app>
2026-04-06 19:05:28 -0400

    Update study-notifier to 1.0.4

--- Casks/study-notifier.rb
+++ Casks/study-notifier.rb
@@ -1,12 +1,12 @@
 cask "study-notifier" do
-  version "1.0.3"
+  version "1.0.4"
 
   if Hardware::CPU.arm?
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-arm64.dmg"
-    sha256 "629e014ebbdeccac3826004d57ba6893faee5a776f269227399d5d0fa85787b4"
+    sha256 "5e3acf25b01b7105fff5dbdafe58ce3a3ecc13d6a7ace1ab178d3bc9ec2333d2"
   else
     url "https://github.com/tanner-doorlist/research/releases/download/v#{version}/Study-Notifier-#{version}-x64.dmg"
-    sha256 "a337e2d5b735978e2e6a4d821668f0a116d831d100109137dfcfff0f9967f159"
+    sha256 "c66396dadc0afccff9ff1f2a97b9986b884cc9bade6d17384bd0ce98d0babf71"
   end
 
   name "Study Notifier"
--- Formula/log-knowledge.rb
+++ Formula/log-knowledge.rb
@@ -1,9 +1,9 @@
 class LogKnowledge < Formula
   desc "CLI for logging engineering knowledge to Knowledge Scribe"
   homepage "https://github.com/tanner-doorlist/research"
-  url "https://github.com/tanner-doorlist/research/releases/download/v1.0.3/logKnowledge"
+  url "https://github.com/tanner-doorlist/research/releases/download/v1.0.4/logKnowledge"
   sha256 "9cb154d0bb7f9b730c2fc32dfb7dddb720c776138e4e80e360fe7c95f5a251ac"
-  version "1.0.3"
+  version "1.0.4"
   license "MIT"
 
   depends_on "curl"
